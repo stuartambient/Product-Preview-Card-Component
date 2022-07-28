8db893f2c5e5696beb011d466d0485db6ff9261f
stuartambient <sambient@gmail.com>
2022-07-28 16:21:37 -0600

    Changed to using classes

---
+++ test.js
@@ -0,0 +1,15 @@
+/* 
+-MAIN
+  <DIV .COMPONENT>
+    IMG
+    <DIV .CONTENT-WRAPPER>
+      <SPAN>
+      <H1>
+      <P>
+      <DIV .PRICE>
+        <H1>
+        <P>
+      <BUTTON><IMG (ICON) </BUTTON>
+
+
+*/
